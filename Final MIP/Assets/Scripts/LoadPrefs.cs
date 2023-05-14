933fb13273a7ef88ea0885f1267a19183729a898
xuanlinnnn <128580016+xuanlinnnn@users.noreply.github.com>
2023-05-14 10:18:39 +0800

    main menu

---
+++ Final MIP/Assets/Scripts/LoadPrefs.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoadPrefs : MonoBehaviour
+{
+    [Header("General Settings")]
+    [SerializeField] private bool canUse = false;
+    [SerializeField] private MenuController menuController;
+
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+    [Header("Quality Level Settings")]
+    [SerializeField] private TMP_Dropdown qualityDropdown;
+
+    [Header("Sensitivty Settings")]
+    [SerializeField] private TMP_Text controllerSenTextValue = null;
+    [SerializeField] private Slider controllerSensSlider = null;
+
+    private void Awake()
+    {
+        if (canUse)
+        {
+            if (PlayerPrefs.HasKey("masterVolume"))
+            {
+                float localVolume = PlayerPrefs.GetFloat("masterVolume");
+
+                volumeTextValue.text = localVolume.ToString("0.0");
+                volumeSlider.value = localVolume;
+                AudioListener.volume = localVolume;
+            }
+
+            if (PlayerPrefs.HasKey("masterQuality"))
+            {
+                int localQuality = PlayerPrefs.GetInt("masterQuality");
+                qualityDropdown.value = localQuality;
+                QualitySettings.SetQualityLevel(localQuality);
+            }
+
+            if (PlayerPrefs.HasKey("masterSen"))
+            {
+                float localSensitivity = PlayerPrefs.GetFloat("masterSen");
+
+                controllerSenTextValue.text = localSensitivity.ToString("0.0");
+                controllerSensSlider.value = localSensitivity;
+                menuController.mainControllerSen = Mathf.RoundToInt(localSensitivity);
+            }
+        }
+    }
+}
